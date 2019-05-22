a8f4d985dadf42180bc693225ecfda6cdf419c0e
Magda <Magda@139.174.105.84>
2019-05-22 10:25:59 +0200

    Merge

--- GIT/src/HelloWorld.java
+++ GIT/src/HelloWorld.java
@@ -4,10 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 	
 	System.out.println("Hello World 1");
-<<<<<<< HEAD
 	System.out.println("Hello World 6");
-=======
-	System.out.println("Hello World 3");
->>>>>>> branch 'master' of https://github.com/nobsonponte/Aufgabe-1.git
 	}
 }
